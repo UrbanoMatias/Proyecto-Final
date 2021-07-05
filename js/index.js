0bd25ba09e280d6388e7ef5f541c5b4811ee9d0b
UrbanoMatias <86387362+UrbanoMatias@users.noreply.github.com>
2021-07-05 18:14:14 -0300

    Agregue animaciones con JS, acomode padding, margin, font, espaciado, h2.

---
+++ js/index.js
@@ -0,0 +1,41 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".navbar", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "-100px",
+});
+
+sr.reveal(".animacionC", {
+    duration: 3000,
+    origin: "right",
+    distance: "-100px",
+});
+sr.reveal(".animacionB", {
+    duration: 4000,
+    origin: "bottom",
+    distance: "-100px",
+});
+sr.reveal(".animacionT", {
+    duration: 5000,
+    origin: "top",
+    distance: "-100px",
+});
+
+sr.reveal(".presentacionL", {
+    duration: 4000,
+    origin: "left",
+    distance: "-100px",
+});
+
+sr.reveal(".presentacionR", {
+    duration: 4000,
+    origin: "right",
+    distance: "-100px",
+});
+
+sr.reveal(".footerAnimacion", {
+    duration: 2000,
+    origin: "top",
+    distance: "-100px",
+});
